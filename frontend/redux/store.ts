a08177e8e0e38849a66b4527be29ffc2cd3e3432
Kipronokirui <victorkipronokirui2@gmail.com>
2023-07-02 20:45:05 +0300

    RTK query

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
+import { apiSlice } from "./services/apiSlice";
 
 export const makeStore = () => 
     configureStore({
         reducer: {
+            [apiSlice.reducerPath]:apiSlice.reducer,
             auth: authReducer
         },
         devTools:process.env.NODE_ENV !== 'production'
